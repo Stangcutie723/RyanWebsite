3b509e0c396e106c445f3b0ff109383a9fd2c397
Stangcutie723 <Stangcutie723@gmail.com>
2013-09-03 18:54:36 -0700

    getting started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RyanWebsite::Application.config.secret_key_base = '37c8a890ca64ebb7860424f6718009a93a14da908f9a58804b2be0a3837f1a51aa8875133fe17421d07cd9953e355ee7427cd4f222a9c27c9c160b69133af59f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RyanWebsite::Application.config.session_store :cookie_store, key: '_RyanWebsite_session'
